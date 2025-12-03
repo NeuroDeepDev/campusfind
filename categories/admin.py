1086645447a9f1233370a6dbb3033c8e4b3464b7
Deepanshu kumar <deepanshuvadav@gmail.com>
2025-12-03 15:44:37 +0000

    chore: remove all files to start from scratch

--- categories/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from categories.models import Category
-
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at')
-    search_fields = ('name',)
-    readonly_fields = ('created_at',)
--- locations/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from locations.models import Location
-
-
-@admin.register(Location)
-class LocationAdmin(admin.ModelAdmin):
-    list_display = ('building_name', 'building_code', 'created_at')
-    search_fields = ('building_name', 'building_code')
-    readonly_fields = ('created_at',)
